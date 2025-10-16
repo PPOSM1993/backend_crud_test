e8ee5a932ecb294a670613aa02ae8001b3625f39
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-10-16 14:26:43 -0300

    Prueba Tecnica para postulacion a empresa como desarrollador backend

---
+++ apps/crud/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class Customer(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=20)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=200)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.IntegerField()
+    created_at = models.DateTimeField(auto_now_add=True)
