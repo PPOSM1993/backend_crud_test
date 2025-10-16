3ef17e60419a5b1954f6a3b414dcd9bdbd6f8ecc
Pedro Pablo Osorio San Martin <pedro.osorio04@inacapmail.com>
2025-10-16 14:28:17 -0300

    Se sube la prueba tecnica

--- apps/crud/models.py
+++ apps/crud/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Customer(models.Model):
-    first_name = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=100 )
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=20)
